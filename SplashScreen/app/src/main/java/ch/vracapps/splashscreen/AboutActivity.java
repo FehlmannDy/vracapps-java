c98cd0cf71bb4d75147a373aac6431c9e3c0fa1d
FehlmannDy <dylan.fehlmann@rpn.ch>
2020-01-08 21:57:04 +0100

    Fix (AboutActivity) : fix ic_arrow up and down

--- SplashScreen/app/src/main/java/ch/vracapps/splashscreen/AboutActivity.java
+++ SplashScreen/app/src/main/java/ch/vracapps/splashscreen/AboutActivity.java
@@ -2,7 +2,9 @@ package ch.vracapps.splashscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.util.List;
 
 import ch.vracapps.splashscreen.model.About_Classes.About;
 import ch.vracapps.splashscreen.model.About_Classes.AboutCategory;
+import iammert.com.expandablelib.ExpandCollapseListener;
 import iammert.com.expandablelib.ExpandableLayout;
 import iammert.com.expandablelib.Section;
 
@@ -22,18 +25,36 @@ public class AboutActivity extends AppCompatActivity {
 
         ExpandableLayout layout = (ExpandableLayout)findViewById(R.id.expandable_layout);
 
+
         layout.setRenderer(new ExpandableLayout.Renderer<AboutCategory, About>() {
 
             @Override
             public void renderParent(View view, AboutCategory aboutCategory, boolean isExpand, int parentPosition) {
                 ((TextView)view.findViewById(R.id.tv_parent_name)).setText(aboutCategory.name);
-                view.findViewById(R.id.arrow).setBackgroundResource(isExpand?R.drawable.ic_arrow_up:R.drawable.ic_arrow_down);
+                ((ImageView)view.findViewById(R.id.arrow)).setImageResource(R.drawable.ic_arrow_up);
             }
 
             @Override
             public void renderChild(View view, About about, int parentPosition, int childPosition) {
                 ((TextView)view.findViewById(R.id.tv_child_name)).setText(about.name);
 
+            }
+        });
+
+        layout.setExpandListener(new ExpandCollapseListener.ExpandListener<AboutCategory>() {
+            @Override
+            public void onExpanded(int i, AboutCategory aboutCategory, View view) {
+                //layout expanded
+                ((ImageView)view.findViewById(R.id.arrow)).setImageResource(R.drawable.ic_arrow_down);
+            }
+        });
+
+        layout.setCollapseListener(new ExpandCollapseListener.CollapseListener<AboutCategory>() {
+
+            @Override
+            public void onCollapsed(int i, AboutCategory aboutCategory, View view) {
+                //layout collapsed
+                ((ImageView)view.findViewById(R.id.arrow)).setImageResource(R.drawable.ic_arrow_up);
             }
         });
 
